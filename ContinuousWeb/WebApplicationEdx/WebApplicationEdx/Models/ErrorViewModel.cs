c80553a2bd3b023bc7e2d7c7a4b4e388642a8cab
OlegStetsula <olegstetsula@gmail.com>
2018-11-14 09:50:44 +0200

    firstCommit

---
+++ ContinuousWeb/WebApplicationEdx/WebApplicationEdx/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationEdx.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
